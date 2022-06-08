e01a46b1068f62b67cda2a6c42dd4f7a733f483c
danehermansen <103455414+danehermansen@users.noreply.github.com>
2022-06-07 18:03:48 -0600

    Update weather.py

--- practical_python/weather.py
+++ practical_python/weather.py
@@ -1,6 +1,6 @@
 import requests
 def get_weather_desc_temp():
-    api_key = '0edcc78385d9ac0b141f18d72c2ed636'
+    
     city = 'Orlando'
     url = 'https://api.openweathermap.org/data/2.5/weather?q=city&appid=api_key'
 
